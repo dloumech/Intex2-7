0551b8d4b86877761f22620f8b3f3024dfec1a67
Sydney Hull <sydneyjhull@gmail.com>
2023-04-10 21:28:18 -0600

    here ya go hunny

--- WebApplication1/Models/fagelgamous_databaseContext.cs
+++ WebApplication1/Models/fagelgamous_databaseContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 using Microsoft.EntityFrameworkCore.Metadata;
 
 #nullable disable
@@ -57,7 +58,6 @@ namespace INTEXII.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseNpgsql("Host=localhost;Database=fagelgamous_database;Username=postgres;Password=password");
             }
         }

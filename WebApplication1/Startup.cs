b0945b1d41d6364f908a9f35234c95e70159bd6d
Sydney Hull <sydneyjhull@gmail.com>
2023-04-10 22:05:55 -0600

    he

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -95,15 +95,15 @@ namespace WebApplication1
                 await next.Invoke();
             });
 
-            app.UseAuthentication();
+            //app.UseAuthentication();
             app.UseAuthorization();
 
-            app.Use(async (context, next) =>
-            {
+            //app.Use(async (context, next) =>
+            //{
                 //csp header type and the information we are adding to the CSP header
                 //context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'");
                 //await next();
-            });
+            //});
 
             app.UseEndpoints(endpoints =>
             {

99af0d353876586f14f32a19b08f4348e70ffe68
Darby Mecham <dloumech@byu.edu>
2023-04-10 21:29:00 -0600

    idk what I changed here tbh

--- WebApplication1/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ WebApplication1/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System;
+using WebApplication1.Data;
 
 namespace INTEXII.Data.Migrations
 {
